4a27f35c3c103d14d70095539bb3977f696a9d0f
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2026-08-27 17:12:32 +0200

    Fix trailing slash in base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rubenterre.github.io',
-  base: '/bibliogames',
+  base: '/bibliogames/',
 });
